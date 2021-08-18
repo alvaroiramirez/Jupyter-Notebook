dd3f04fadb5913ae38ee896a347be6210a4a515c
Alvaro Ramirez <aramirez@securiport.com>
2021-08-18 17:05:52 -0400

    Added comment

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Terminal
+# ========
+# pip install jupyter_contrib_nbextensions
+# pip install virtualenv
+# python -m pip install --upgrade pip
+# .venv\Scripts\activate
+# pip install numpy
+# pip install opencv-python
+# pip install matplotlib
+# python -m pip install jupyter
+
+# Run Jupyter from CMD
+# ====================
+# jupyter notebook
